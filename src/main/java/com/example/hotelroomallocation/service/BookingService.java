a8201c2be0eb269e549b811fcc6460f8dfe258db
JBURYLO <jakub.burylo@gmail.com>
2024-07-24 13:13:15 +0200

    added basic business logic

--- src/main/java/com/example/hotelroomallocation/service/BookingService.java
+++ src/main/java/com/example/hotelroomallocation/service/BookingService.java
@@ -4,10 +4,37 @@ import com.example.hotelroomallocation.model.BookingRequest;
 import com.example.hotelroomallocation.model.BookingResponse;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BookingService {
 
     public BookingResponse optimizeOccupancy(BookingRequest bookingRequest) {
-        return new BookingResponse();
+        List<Double> potentialGuests =  bookingRequest.getPotentialGuests();
+
+        List<Double> potentialPremiumGuests = potentialGuests.stream()
+                .filter(guest -> guest >= 100.00)
+                .toList();
+
+        double revenuePremium = potentialPremiumGuests.stream()
+                .mapToDouble(Double::doubleValue)
+                .sum();
+
+        List<Double> potentialEconomyGuests = potentialGuests.stream()
+                .filter(guest -> guest < 100.00)
+                .toList();
+
+        double revenueEconomy = potentialEconomyGuests.stream()
+                .mapToDouble(Double::doubleValue)
+                .sum();
+
+        BookingResponse bookingResponse = new BookingResponse();
+
+        bookingResponse.setUsagePremium(potentialPremiumGuests.size());
+        bookingResponse.setRevenuePremium(revenuePremium);
+        bookingResponse.setUsageEconomy(potentialEconomyGuests.size());
+        bookingResponse.setRevenueEconomy(revenueEconomy);
+
+        return bookingResponse;
     }
 }
