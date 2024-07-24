540ba462c7531cea98921e998883e847298dc1e6
JBURYLO <jakub.burylo@gmail.com>
2024-07-24 13:25:48 +0200

    added failing test case

--- src/test/java/com/example/hotelroomallocation/service/BookingServiceTest.java
+++ src/test/java/com/example/hotelroomallocation/service/BookingServiceTest.java
@@ -35,4 +35,22 @@ public class BookingServiceTest {
         assertThat(bookingResponse.getUsageEconomy()).isEqualTo(4);
         assertThat(bookingResponse.getRevenueEconomy()).isEqualTo(189.99);
     }
+
+    @Test
+    void whenBookingRequestIsReceived_andThereIsFewerEconomyRoomsThanPotentialEconomyGuests_itIsOptimized() {
+        // GIVEN
+        BookingRequest bookingRequest = new BookingRequest();
+        bookingRequest.setPremiumRooms(9);
+        bookingRequest.setEconomyRooms(4);
+        bookingRequest.setPotentialGuests(Arrays.asList(23.0, 45.0, 155.0, 374.0, 22.0, 35.0, 99.99, 100.0, 101.0, 115.0, 209.0, 77.8));
+
+        // WHEN
+        BookingResponse bookingResponse = bookingService.optimizeOccupancy(bookingRequest);
+
+        // THEN
+        assertThat(bookingResponse.getUsagePremium()).isEqualTo(8);
+        assertThat(bookingResponse.getRevenuePremium()).isEqualTo(1231.79);
+        assertThat(bookingResponse.getUsageEconomy()).isEqualTo(4);
+        assertThat(bookingResponse.getRevenueEconomy()).isEqualTo(125);
+    }
 }
