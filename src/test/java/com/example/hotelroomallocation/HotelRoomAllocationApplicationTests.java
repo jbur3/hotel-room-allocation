cf7c8970de194d5f10389783c86d09204170b1e3
JBURYLO <jakub.burylo@gmail.com>
2024-07-24 11:03:18 +0200

    initial commit

---
+++ src/test/java/com/example/hotelroomallocation/HotelRoomAllocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotelroomallocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelRoomAllocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
